945fe27288b2ec69bab8e36eac53f55a26d91677
tvancisin <tomasvancisin@gmail.com>
2023-06-26 00:01:53 +0100

    fifth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,14 @@ export default function Home() {
         <div className="w-full md:w-1/4 md:pr-10 order-3 md:order-1">
           <div className="max-w-md md:float-right md:text-right leading-loose tracking-tight md:sticky md:top-0 ">
             <p className="text-3xl my-4 md:my-5">Tomas Vancisin</p>
-            <p className="text-base my-4 md:my-5">PhD researcher at the School of Computer Science St Andrews<br />
-              <a href="https://sachi.cs.st-andrews.ac.uk/" className="text-blue-990 hover:text-blue-800 visited:text-purple-600">St Andrews HCI Research Group (SACHI)</a></p>
-            <p className="text-xl my-4 md:my-5">Keywords</p>
-            <p className="my-4 md:my-5 text-sm">Digital Humanities<br />Information Visualization <br /> Digital Heritage <br /> </p>
+            <p className="text-sm my-4 md:my-5">Postgraduate Research Assistant<br />School of History <br />University of St Andrews </p>
+            <p className="text-sm my-4 md:my-5">PhD researcher <br /> School of Computer Science <br /> University of St Andrews<br />
+              <a href="https://sachi.cs.st-andrews.ac.uk/" className="text-blue-990 hover:text-blue-800 visited:text-purple-600">St Andrews HCI Research Group (SACHI)</a>
+            </p>
+
+            {/* <p className="text-xl my-4 md:my-5">Keywords</p> */}
+            <p className="md:my-4 text-sm">Digital Humanities<br />Information Visualization <br /> Digital Heritage <br /> </p>
+            <img className="inline-block" src="/uni_logo.png" />
 
             {/* <ul className="flex flex-wrap justify-between flex-col">
               <li><a href="#visualization" className="nav">Visualization</a></li>
@@ -20,33 +24,101 @@ export default function Home() {
             </ul> */}
           </div>
         </div>
+
         <div className="w-full md:w-3/4 order-1 md:order-2">
           <div className="max-w-[50vw] leading-loose tracking-tight">
-            <h1 className="font-bold my-12 text-center">Research & Publications</h1>
-            <p className="mb-8">This template is inspired by the amazing <a href="https://minimalissimo.shop/product/minimalist-writer-ii" target="_blank">https://minimalissimo.shop/product/minimalist-writer-ii</a> created by <a href="https://manuelmoreale.com/">Manuel Moreale</a>.</p>
-            <p className="mb-8">Lorem ipsum dolor sit amet, consectetur <a href="#">random link</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Vitae aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis mattis aliquam. In est ante in nibh mauris. Egestas congue quisque egestas diam in. Facilisi nullam vehicula ipsum a arcu. Nec nam aliquam sem et tortor consequat. Eget mi proin sed libero enim sed faucibus turpis in. Hac habitasse platea dictumst quisque. In aliquam sem fringilla ut. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Accumsan lacus vel facilisis volutpat est velit egestas dui id. At tempor commodo ullamcorper a. Volutpat commodo sed egestas egestas fringilla. Vitae congue eu consequat ac.</p>
-            <p className="mb-8" id="visualization">Netus et malesuada fames ac turpis egestas sed. Sociis natoque penatibus et magnis dis. Pulvinar etiam non quam lacus suspendisse faucibus. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Eget nullam non nisi est sit amet facilisis. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Vulputate mi sit amet mauris commodo quis. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Enim blandit volutpat maecenas volutpat blandit. Scelerisque fermentum dui faucibus in ornare. Ultricies tristique nulla aliquet enim tortor at auctor urna nunc. Id interdum velit laoreet id. Ut lectus arcu bibendum at varius vel pharetra vel turpis.</p>
-            <a href="https://provenance.tomasvancisin.co.uk">
-              <img className="opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/scroll.PNG" />
-            </a>
-            {/* <iframe src="https://provenance.tomasvancisin.co.uk" className="w-full h-full" /> */}
-            <p className="mb-8">A diam sollicitudin tempor id eu. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet facilisis magna etiam tempor. Non diam phasellus vestibulum lorem sed risus. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Quam quisque id diam vel quam elementum pulvinar etiam. Eu volutpat odio facilisis mauris. Molestie ac feugiat sed lectus vestibulum mattis. In vitae turpis massa sed elementum tempus egestas sed sed. Sed id semper risus in hendrerit gravida rutrum quisque non. Quis lectus nulla at volutpat diam ut venenatis. Ultrices eros in cursus turpis massa tincidunt dui. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Lobortis mattis aliquam faucibus purus. Egestas sed tempus urna et pharetra pharetra. Tellus pellentesque eu tincidunt tortor aliquam nulla.</p>
+            <h1 className="text-2xl my-5 text-center">RESEARCH</h1>
 
-            <p className="mb-8">Cras fermentum odio eu feugiat pretium nibh ipsum. Integer vitae justo eget magna fermentum iaculis eu non diam. Tellus molestie nunc non blandit massa enim nec dui. Ullamcorper morbi tincidunt ornare massa eget egestas. Orci a scelerisque purus semper eget duis at tellus at. Consequat id porta nibh venenatis cras sed. Enim eu turpis egestas pretium. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Dui vivamus arcu felis bibendum. Eu consequat ac felis donec et odio. Ultrices gravida dictum fusce ut placerat orci nulla. Leo urna molestie at elementum eu facilisis. Vulputate mi sit amet mauris commodo. Eu consequat ac felis donec et odio pellentesque.</p>
-            <a href="https://graduates.tomasvancisin.co.uk">
-              <img className="opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/graduates.JPG" />
-            </a>
+            {/* <p className="mb-8 text-base">
+              My name is Tom and I work with 
+              Digital Humanities, Information Visualication, and
+              Digital Heritage.
+            </p> */}
+
+
+            <h1 className="text-xl my-2 text-left">Legacies of the Empire <p className="text-xs">School of History, University of St Andrews</p></h1>
+            <div className="mb-8 text-sm"> Currently working as
+              a postgraduate research assistant for the Legacies of the Empire Project,
+              exploring how the University of St Andrews has benefited from UK's colonial past.
+              Using visualization to map careers and occupations of St Andrews University alumni in British colonies.
+              <div className="grid grid-cols-1 justify-items-center">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/legacies.png" /></div>
+                <div className="text-xs mt-2">St Andrews students travelling to
+                  and from British colonies [1700 - 1897].</div>
+              </div>
+            </div>
+
+            <h1 className="text-xl my-2 text-left">PhD Research<p className="text-xs">School of Computer Science, University of St Andrews</p></h1>
+            <div className="mb-8 text-sm">
+              Developed a large variety of visualizations to explore the Biographical Register
+              of the University of St Andrews; a collection of 25,525 student and staff records
+              spanning the period between 1579 and 1897.
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/vanc.JPG" /></div>
+                <div className="text-xs mt-2">Using Tableau to visualize students' birth locations and college affiliation [1747 - 1897].</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/graduates.JPG" /></div>
+                <div className="text-xs mt-2">Using D3, Leaflet and Marker-Clustering to visualize graduates of the University [1747 - 1897].</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/split.JPG" /></div>
+                <div className="text-xs mt-2">Emphasizing differences between current political map and a map from the period when
+                  the British empire was at its greatest.</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/geojson.jpg" /></div>
+                <div className="text-xs mt-2">Using GeoJSON and D3 to map the development of academic networks.</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/round_bundle.JPG" /></div>
+                <div className="text-xs mt-2">Exploring career locations network through edge-bundling in D3.</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/map_network.JPG" /></div>
+                <div className="text-xs mt-2">Exploring student paths to other universities in the UK and Europe using Leaflet and Echarts.</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/bars.JPG" /></div>
+                <div className="text-xs mt-2">Visualizing the transformations of the University records since 1888 to 2022</div>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/scroll.PNG" /></div>
+                <div className="text-xs mt-2">Visualizing the transformations of the University records since 1888 to 2022</div>
+              </div>
+              {/* <p className="mb-8" id="visualization"></p>
+            <figure className="mb-8">
+              <a href="https://provenance.tomasvancisin.co.uk">
+                <img className="rounded-md opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/scroll.PNG" />
+                <figcaption className="text-sm text-center mt-2"> [1700 - 1897].</figcaption>
+              </a>
+            </figure> */}
+              {/* <iframe src="https://provenance.tomasvancisin.co.uk" className="w-full h-full" /> */}
+
+            </div>
+
+            <h1 className="text-xl my-2 text-left">Lost Detectives<p className="text-xs">School of Modern Languages, University of St Andrews</p></h1>
+            <div className="mb-8 text-sm">
+              Worked as a research assistant for the Knowledge Exchange and Impact project which aims to bring works of
+              nineteenth-century Russian crime fiction to greater public prominence through adaptation.
+              <div className="grid grid-cols-1 justify-items-center mb-8">
+                <div><img className="rounded-md md:mt-5 opacity-80 hover:scale-105 hover:opacity-100 transition-all" src="/detectives.png" /></div>
+                <div className="text-xs mt-2">Illustration (Carol Adlam) and the landing page for the Lost Detectives projects developed by me.</div>
+              </div>
+            </div>
 
           </div>
+
           <div className="w-full md:w-1/2 order-2 md:order-4">
             <div className="max-w-md leading-loose tracking-tight">
               <p className="font-bold my-4 md:my-12">About Me</p>
-
               <p className="mb-8">Arcu risus quis varius quam quisque id diam vel. Consectetur adipiscing elit ut aliquam purus sit amet. Nibh tortor id aliquet lectus proin nibh. </p>
             </div>
           </div>
+
         </div>
+
       </div>
-    </div >
+    </div>
   )
 }
