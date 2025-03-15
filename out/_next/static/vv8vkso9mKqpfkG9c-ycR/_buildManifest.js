e39248fa52a4b0bf7c9579d3f948a0940b61d015
Tomas Vancisin <tvancisi@ed.ac.uk>
2025-03-15 19:08:21 +0000

    tresting

--- out/_next/static/vv8vkso9mKqpfkG9c-ycR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b6c55ba9a60d1628.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
