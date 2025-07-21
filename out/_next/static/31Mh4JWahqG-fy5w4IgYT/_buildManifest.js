bf9d631ba98d9d8b085177d8634ef625110cd0b9
Tomas Vancisin <tvancisi@ed.ac.uk>
2025-07-21 20:20:02 +0100

    built mediation

---
+++ out/_next/static/31Mh4JWahqG-fy5w4IgYT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dcdc84aa543499e3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
