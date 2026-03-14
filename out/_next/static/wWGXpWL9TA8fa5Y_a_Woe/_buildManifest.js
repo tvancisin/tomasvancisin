574165cb5494d8924b367c0d1f656b2d95501777
Tomas Vancisin <tomasvancisin@gmail.com>
2026-03-14 15:55:47 +0000

    fix

--- out/_next/static/wWGXpWL9TA8fa5Y_a_Woe/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:out/_next/static/wWGXpWL9TA8fa5Y_a_Woe/_buildManifest.js
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2e2c9463c1514abe.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
-========
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6eb1b235812b8237.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
->>>>>>>> 3b7e086 (update march):out/_next/static/S4z5iE3dXGx9gZ8-3Szx0/_buildManifest.js
